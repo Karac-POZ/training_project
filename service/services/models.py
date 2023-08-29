159a8d0b54b171428f8a091592134da01a9047e9
Rasul Shaidullinov <rasul200601@gmail.com>
2023-08-29 22:47:41 +0600

    4 - 2

--- service/services/models.py
+++ service/services/models.py
@@ -2,7 +2,7 @@ from django.core.validators import MaxValueValidator
 from django.db import models
 
 from clients.models import Client
-from services.tasks import set_price
+from services.tasks import set_price, set_comment
 
 
 class Service(models.Model):
@@ -17,6 +17,7 @@ class Service(models.Model):
         if self.full_price != self.__full_price:
             for sub in self.subscription.all():
                 set_price.delay(sub.id)
+                set_comment.delay(sub.id)
 
         return super().save(*args, **kwargs)
 
@@ -46,6 +47,7 @@ class Plan(models.Model):
         if self.discount_percent != self.__discount_percent:
             for sub in self.subscription.all():
                 set_price.delay(sub.id)
+                set_comment.delay(sub.id)
 
         return super().save(*args, **kwargs)
 
@@ -58,6 +60,7 @@ class Subscription(models.Model):
     service = models.ForeignKey(Service, related_name='subscription', on_delete=models.PROTECT)
     plan = models.ForeignKey(Plan, related_name='subscription', on_delete=models.PROTECT)
     price = models.PositiveIntegerField(default=0)
+    comment = models.CharField(max_length=50, default='')
 
     def __str__(self):
         return f'{self.client.user}: {self.service.name}/{self.plan.plan_type}'
