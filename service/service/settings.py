8cd325bdd4b016445105b2e71b0d89db1ef8f3ad
Rasul Shaidullinov <rasul200601@gmail.com>
2023-08-31 00:25:17 +0600

    5 - 1

--- service/service/settings.py
+++ service/service/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'cachalot',
+
     'clients',
     'services',
 ]
@@ -152,3 +154,12 @@ LOGGING = {
 }
 
 CELERY_BROKER_URL = 'redis://redis:6379/0'
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis:6379/1",
+    }
+}
+
+PRICE_CACHE_NAME = 'price_cache'
